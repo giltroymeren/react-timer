73f75a5d23af277780dbee451bb3bb661750dfb2
Giltroy P. Meren <giltroy.meren@GFI00245.local>
2021-12-31 18:32:28 +0800

    Implemented all minimum features

--- src/components/timer/index.js
+++ src/components/timer/index.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./index.css";
 import PropTypes from 'prop-types'
 
 const Timer = ({ initial }) => {
+  const [current, setCurrent] = useState(initial)
+
+  let myTimer = null
+  useEffect(() => {
+    if(current === 0) return
+
+    myTimer = setTimeout(() => setCurrent(current-1), 1000)
+  })
+
+  const stopTimer = () => clearTimeout(myTimer)
+
   return (
     <div className="mt-100 layout-column align-items-center justify-content-center" >
-      <div className="timer-value" data-testid="timer-value">{initial}</div>
-      <button className="large" data-testid="stop-button">Stop Timer</button>
+      <div className="timer-value" data-testid="timer-value">{current}</div>
+      <button className="large" data-testid="stop-button" onClick={stopTimer}>Stop Timer</button>
     </div >
   )
 }
