6327c042e0fd0c0863a9aac505d5ec785593cda3
Giltroy P. Meren <giltroy.meren@GFI00245.local>
2021-12-31 16:50:27 +0800

    Initial UI is rendered as expected with initial timer value

--- src/components/timer/index.js
+++ src/components/timer/index.js
@@ -1,15 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import "./index.css";
+import PropTypes from 'prop-types'
 
-export default class Timer extends Component {
-
-  render() {
-    return (
-      <div className="mt-100 layout-column align-items-center justify-content-center">
-         <div className="timer-value" data-testid="timer-value"></div>
-         <button className="large" data-testid="stop-button">Stop Timer</button>
-      </div>
-    );
-  }
+const Timer = ({ initial }) => {
+  return (
+    <div className="mt-100 layout-column align-items-center justify-content-center" >
+      <div className="timer-value" data-testid="timer-value">{initial}</div>
+      <button className="large" data-testid="stop-button">Stop Timer</button>
+    </div >
+  )
 }
 
+Timer.propTypes = {
+  initial: PropTypes.number.isRequired,
+}
+
+export default Timer
